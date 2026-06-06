21345c2ad560539659c0cc8c7b2b7956eb873b94
Cheeseday <aleksandrkirilushkin@gmail.com>
2026-06-06 23:40:56 +0200

    feat: analysis of data quality

---
+++ etl/constants.py
@@ -0,0 +1,11 @@
+EUROPEAN_COUNTRIES = [
+    # EU 27
+    'AUT', 'BEL', 'BGR', 'HRV', 'CYP', 'CZE', 'DNK', 'EST', 'FIN',
+    'FRA', 'DEU', 'GRC', 'HUN', 'IRL', 'ITA', 'LVA', 'LTU', 'LUX',
+    'MLT', 'NLD', 'POL', 'PRT', 'ROU', 'SVK', 'SVN', 'ESP', 'SWE',
+    # Non-EU Europe
+    'BLR', 'GEO', 'GBR', 'UKR', 'CHE', 'NOR', 'ALB',
+]
+
+DECOUPLING_START_YEAR = 1990
+DECOUPLING_END_YEAR = 2023
